7cefef725ea257afec0461d35753c9dd16c79291
Young Cheeks <youngcheeks@Youngs-MacBook-Air.local>
2021-05-27 15:37:25 -0400

    Just about v1

--- src/banners/_banner_template/assets/js/main.js
+++ src/banners/_banner_template/assets/js/main.js
@@ -1,5 +1,4 @@
-const tl = anime.timeline({
-  easing: 'easeInOutQuad',
-})
-
-tl.add({})
+// GSAP - https://greensock.com/docs/v3
+var tl = gsap.timeline({repeat: 3, repeatDelay: 1});
+tl.to("", {});
+        
